4b578f93982001387d72e061a25e52759d697f2b
mannat0711-tech <tharejateshika@gmail.com>
2026-01-26 18:19:57 +0530

    Create program2.cpp

---
+++ program2.cpp
@@ -0,0 +1,43 @@
+//float and integers 
+#include<iostream>
+//using namespace std;
+int main()
+{
+    int a=10;
+    float f=14.67;
+    //pritnf("the value of a=%d the value of f=%f,a,f")
+    std::cout<<"the value of a="<<a<<std::endl<<"the value of f="<<f;
+    std::cout<<"\nhello world";
+    return 0;
+}
+
+//float character and integer 
+#include<iostream>
+//using namespace std;
+int main()
+{
+    int a=10;
+    float f=12.5;
+    char c='z';
+    //pritnf("the value of a=%d the value of f=%f,a,f")
+    std::cout<<"the value of a="<<a<<std::endl<<"the value of f="<<f<<std::endl<<"the value of c="<<c;
+    std::cout<<"\nhello world";
+    
+    return 0;
+}
+
+//float character and integer 
+#include<iostream>
+//using namespace std;
+int main()
+{
+    int a;
+    float f;
+    char c;
+    //printf("the value of a=%d the value of f=%f,a,f")
+    //scanf("%d,%f,%c,&a,&b.&c")
+    std::cin>>a>>f>>c;
+    std::cout<<"the value of a="<<a<<std::endl<<"the value of f="<<f<<std::endl<<"the value of c="<<c;
+    std::cout<<"\nhello world";
+    return 0;
+}
