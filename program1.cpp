53c90345685e852ba0c5ea4b4cf772de5d164de3
mannat0711-tech <tharejateshika@gmail.com>
2026-01-26 18:18:56 +0530

    Create program1.cpp

---
+++ program1.cpp
@@ -0,0 +1,18 @@
+// hello world in c++
+#include<iostream>
+//using namespace std;
+int main()
+{
+    std::cout<<"hello world";
+    return 0;
+}
+
+//we can even use \n
+//hello world with endl
+#include<iostream>
+//using namespace std;
+int main()
+{
+    std::cout<<"hello"<<std::endl<<"world";
+    return 0;
+}
