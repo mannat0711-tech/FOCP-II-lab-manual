78a836280fdc0c203bb7f4f815c34a1292968f3f
mannat0711-tech <tharejateshika@gmail.com>
2026-01-26 18:20:43 +0530

    Create string.cpp

---
+++ string.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+//using namespace 
+int main()
+{
+    std::string name="FOCP II";
+    std::cout<<" value of name "<<name;
+    return 0;
+}  
+
+#include<iostream>
+//using namespace
+int main()
+{
+    std::string name;
+ std::cin>>name;
+    std::cout<<"name:"<<name;
+    return 0;
+}
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    float f;
+
+    string name="focp II";
+    cout<<"enter the value of a,f,name";
+    cin>>a;
+    cin>>f;
+    //cin.ignore();
+    //cin>>name;
+    cin>>name;
+    cout<<"\n a="<<a<<"\n f="<<f<<"\n name="<<name;
+    return 0;
+}
