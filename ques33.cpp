e095e452b474d825136daefcfcd65e2f00875723
mannat0711-tech <tharejateshika@gmail.com>
2026-02-03 21:10:15 +0530

    Create ques33.cpp

---
+++ ques33.cpp
@@ -0,0 +1,23 @@
+/*Q33. A grading system stores marks of multiple students in arrays. Implement a solution to accept
+marks in 5 subjects for 3 students, then display:
+• marks in 2nd subject of 1st student, and
+• marks in 5th subject of 3rd student.*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int marks[3][5];
+
+    // input marks
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 5; j++) {
+            cin >> marks[i][j];
+        }
+    }
+
+    // output required marks
+    cout << "2nd subject of 1st student = " << marks[0][1] << endl;
+    cout << "5th subject of 3rd student = " << marks[2][4] << endl;
+
+    return 0;
+}
