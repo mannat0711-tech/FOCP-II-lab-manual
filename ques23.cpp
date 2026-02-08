bc0d1dd2689ec05f1fa65be26ff8474866d5a0af
mannat0711-tech <tharejateshika@gmail.com>
2026-02-08 11:37:21 +0530

    lab manual

--- ques23.cpp
+++ ques23.cpp
@@ -1,2 +1,52 @@
 /* Q23. A fireworks display system arranges sparks in butterfly style. Implement a solution to display a butterfly
 star pattern.*/
+//breaking the pattern 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+
+    // upper part
+    for(int i = 1; i <= n; i++) {
+        // left stars
+        for(int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+
+        // spaces
+        for(int j = 1; j <= 2*(n - i); j++) {
+            cout << " ";
+        }
+
+        // right stars
+        for(int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    // lower part
+    for(int i = n; i >= 1; i--) {
+        // left stars
+        for(int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+
+        // spaces
+        for(int j = 1; j <= 2*(n - i); j++) {
+            cout << " ";
+        }
+
+        // right stars
+        for(int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
--- ques25.cpp
+++ ques25.cpp
@@ -1,5 +1,4 @@
-/* Q25. A security system generates list of prime keys within given range. Implement a solution to display all
-prime numbers between two limits.
+/* Q25.  
 A web-based application enforces strong password policies during user registration to improve account security.
 The system requires that every password must satisfy the following conditions:
 • Contain at least one uppercase letter (A–Z)
@@ -9,3 +8,35 @@ The system requires that every password must satisfy the following conditions:
 @ # $ % ! & *
 You are required to develop the code so that the password validation logic can be implemented in the application*/
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    char password[50];
+    int i;
+    int upper = 0, lower = 0, digit = 0, special = 0;
+
+    cout << "Enter password: ";
+    cin >> password;
+
+    for(i = 0; password[i] != '\0'; i++) {
+        char ch = password[i];
+
+        if(ch >= 'A' && ch <= 'Z')
+            upper = 1;
+        else if(ch >= 'a' && ch <= 'z')
+            lower = 1;
+        else if(ch >= '0' && ch <= '9')
+            digit = 1;
+        else if(ch == '@' || ch == '#' || ch == '$' || ch == '%' ||
+                ch == '!' || ch == '&' || ch == '*')
+            special = 1;
+    }
+
+    if(upper == 1 && lower == 1 && digit == 1 && special == 1)
+        cout << "Strong Password";
+    else
+        cout << "Weak Password";
+
+    return 0;
+}
