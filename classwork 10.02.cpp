137fc703eaae2324b038a3d8ec6f8bb76dd40e08
mannat0711-tech <tharejateshika@gmail.com>
2026-02-10 11:10:03 +0530

    Create classwork 10.02.cpp

---
+++ classwork 10.02.cpp
@@ -0,0 +1,115 @@
+/*#include<iostream>
+using namespace std;
+void welcome(void);
+
+void welcome()
+{
+    cout<<"\n welcome agent";
+}
+int main()
+{
+    welcome();
+    return 0;
+}
+*/
+/*
+#include<iostream>
+using namespace std;
+
+int sum(int,int);
+float sum(float,float);
+double sum(double,double);
+int main()
+{
+cout<<endl<<sum(10,20);
+cout<<endl<<sum(10.56,20.89);
+cout<<endl<<sum(10.458,20.897);
+
+return 0;
+}
+int sum(int n1, int n2)
+{
+cout<<"\n int method";
+return n1+n2;
+}
+float sum(float n1,float n2)
+{
+    cout<<"\n float method";
+    return n1+n2;
+}
+double sum(double n1, double n2)
+{
+cout<<"\n double method";
+return n1+n2;
+}
+*/
+/*#include<iostream>
+using namespace std;
+int Result(int,int);
+int Result(int,int,int);
+int Result(int,int,int,int);
+int Result(int,int,int,int,int);
+int main()
+{
+    cout<<"\n Result of 2 subjects  "<<Result(45,58);
+    cout<<"\n Result of 3 subjects  "<<Result(57,68,95);
+    cout<<"\n Result of 4 subjects  "<<Result(78,49,65,77);
+    cout<<"\n Result of 5 subjects  "<<Result(47,69,84,56,75);
+    return 0;}
+
+    int Result(int s1,int s2)
+    {
+        return (s1+s2);
+    }
+int Result (int s1,int s2,int s3)
+{
+    return (s1+s2+s3);
+}
+int Result(int s1,int s2,int s3,int s4)
+  {
+    return(s1+s2+s3+s4);
+   }
+int Result(int s1,int s2,int s3,int s4,int s5)
+   {
+    return (s1+s2+s3+s4+s5);
+
+   }
+*/
+/*
+#include<iostream>
+using namespace std;
+ 
+int Result(int,int,int=0,int=0,int=0);
+int main()
+{
+    cout<<"\n Result of 2 subjects  "<<Result(45,58);
+    cout<<"\n Result of 3 subjects  "<<Result(57,68,95);
+    cout<<"\n Result of 4 subjects  "<<Result(78,49,65,77);
+    cout<<"\n Result of 5 subjects  "<<Result(47,69,84,56,75);
+    return 0;}
+
+int Result(int s1,int s2,int s3,int s4,int s5)
+   {
+    return (s1+s2+s3+s4+s5);
+
+   }
+
+   */
+
+/*
+   #include<iostream>
+   using namespace std;
+   void welcome(string="agent",string="");
+   int main()
+   {
+    welcome();
+    welcome ("mannat");
+    welcome("mannat ","thareja");
+    return 0;
+   }
+   //definition
+   void welcome(string fname,string lname)
+   {
+    cout<<"\n welcome "<<fname<<lname;
+   }
+*/
